27ebaf20507140677fa3b59e7b684cf2582bd071
atanas-hristozov <atanasihristozov@mail.bg>
2023-08-09 11:52:41 +0300

    ListAllTasks

--- src/Commands/Listing/ListAllTasks.java
+++ src/Commands/Listing/ListAllTasks.java
@@ -1,4 +1,25 @@
 package Commands.Listing;
 
-public class ListAllTasks {
+import Commands.Contracts.Command;
+import Core.Contracts.TaskManagementRepository;
+import Models.Tasks.Interfaces.Task;
+import Utils.ListingHelpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAllTasks implements Command {
+    private List<Task> tasks;
+
+    public ListAllTasks(TaskManagementRepository taskManagementRepository) {
+        this.tasks = taskManagementRepository.getTasks();
+    }
+
+    @Override
+    public String execute(List<String> parameters) {
+        if(tasks.isEmpty()) {
+            return "There are no tasks.";
+        }
+        return ListingHelpers.elementsToString(tasks);
+    }
 }
