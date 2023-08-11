bd5df176aa1b3ad66650a494b4a98a4e19ad4033
popito2 <martizlato@abv.bg>
2023-08-11 12:11:41 +0300

    ShowAllTeamBoardsTests implemented

--- src/Commands/ShowAllTeamBoards.java
+++ src/Commands/ShowAllTeamBoards.java
@@ -5,22 +5,20 @@ import Core.Contracts.TaskManagementRepository;
 import Models.interfaces.Team;
 import Utils.ValidationHelpers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ShowAllTeamBoards implements Command {
     public static final int EXPECTED_NUMBER_OF_PARAMETERS = 0;
     private TaskManagementRepository taskManagementRepository;
-    private String name;
 
     public ShowAllTeamBoards(TaskManagementRepository taskManagementRepository) {
-        this.taskManagementRepository = new Core.TaskManagementRepository();
+        this.taskManagementRepository = taskManagementRepository;
     }
 
     @Override
     public String execute(List<String> parameters) {
         ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_PARAMETERS);
-        name = parameters.get(0);
+        String name = parameters.get(0);
         Team team = taskManagementRepository.findTeamByName(name);
         return team.getBoards().toString();
     }
