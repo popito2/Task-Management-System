1a862027d1cef0673a9043a9d90b78907f915d7e
popito2 <martizlato@abv.bg>
2023-08-11 11:31:02 +0300

    ShowAllTeamBoardsTests implemented

--- tests/Commands/ShowAllTheTeamMembersTests.java
+++ tests/Commands/ShowAllTheTeamMembersTests.java
@@ -1,6 +1,7 @@
 package Commands;
 
 import Core.TaskManagementRepository;
+import Models.interfaces.Team;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ShowAllTheTeamMembersTests {
@@ -27,6 +29,26 @@ public class ShowAllTheTeamMembersTests {
         List<String> params = Arrays.asList(new String[2]);;
 
         assertThrows(IllegalArgumentException.class, () -> showAllTheTeamMembers.execute(params));
+    }
+
+    @Test
+    public void should_ShowAllTeamMembers_When_ArgumentsAreValid(){
+        CreateNewTeam createNewTeam = new CreateNewTeam(taskManagementRepository);
+        CreateNewPerson createNewPerson = new CreateNewPerson(taskManagementRepository);
+        AddPersonToTeam addPersonToTeam = new AddPersonToTeam(taskManagementRepository);
+
+        List<String> team = List.of("Banditi");
+        List<String> member = List.of("James");
+        List<String> add = List.of("Banditi", "James");
+
+        createNewTeam.execute(team);
+        createNewPerson.execute(member);
+        addPersonToTeam.execute(add);
+        String result = showAllTheTeamMembers.execute(team);
+
+        Team team1 = taskManagementRepository.findTeamByName("Banditi");
+
+        assertEquals(team1.getMembers().toString(), result);
     }
 
 
