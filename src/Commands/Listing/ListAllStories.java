14b0359871600382bfae951b7da8bfca52cbd13e
popito2 <martizlato@abv.bg>
2023-08-17 19:20:32 +0300

    storing and filtering done in ListAllStories and ListAllBugs

--- src/Commands/Listing/ListAllStories.java
+++ src/Commands/Listing/ListAllStories.java
@@ -2,13 +2,19 @@ package Commands.Listing;
 
 import Commands.Contracts.Command;
 import Core.Contracts.TaskManagementRepository;
+import Models.Tasks.Enums.Priority;
+import Models.Tasks.Enums.Severity;
 import Models.Tasks.Interfaces.Feedback;
 import Models.Tasks.Interfaces.Story;
 import Models.Tasks.Interfaces.Task;
 import Utils.ListingHelpers;
+import Utils.ParsingHelpers;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListAllStories implements Command {
     private List<Story> stories = new ArrayList<>();
@@ -25,10 +31,42 @@ public class ListAllStories implements Command {
             return "There are no registered stories.";
         }
 
-        return ListingHelpers.elementsToString(getFeedbackFromTasks(tasks));
+        List<Story> filteredStories;
+        stories = getStoriesFromTasks(tasks);
+
+        if (parameters.get(0).equals("filter")){
+            if(parameters.get(1).equals("Priority")){
+                filteredStories = stories.stream()
+                        .filter(story -> story.getPriority() == ParsingHelpers.tryParseEnum(parameters.get(2), Priority.class))
+                        .collect(Collectors.toList());
+                return filteredStories.toString();
+            }
+            if(parameters.get(1).equals("Title")){
+                filteredStories = stories.stream()
+                        .filter(story -> story.getTitle() == parameters.get(2))
+                        .collect(Collectors.toList());
+                return filteredStories.toString();
+            }
+        }
+        if (parameters.get(0).equals("sort")){
+            if(parameters.get(1).equals("Title")){
+                List<Story> sortedStories = stories.stream()
+                        .sorted(Comparator.comparing(Story::getTitle))
+                        .collect(Collectors.toList());
+                return sortedStories.toString();
+            }
+            if(parameters.get(1).equals("Assignee")){
+                List<Story> sortedStories = stories.stream()
+                        .sorted(Comparator.comparing(Story::getAssignee))
+                        .collect(Collectors.toList());
+                return sortedStories.toString();
+            }
+        }
+
+        return stories.toString();
     }
 
-    private List<Story> getFeedbackFromTasks(List<Task> tasks){
+    private List<Story> getStoriesFromTasks(List<Task> tasks){
         for (Task task:tasks) {
             if(task instanceof Feedback){
                 stories.add((Story) task);
