4f972674916dfb43b14ebc8643f4cf0cb2fa662b
atanas-hristozov <atanasihristozov@mail.bg>
2023-08-17 19:04:00 +0300

    ListAllTasks

--- src/Commands/Listing/ListAllTasks.java
+++ src/Commands/Listing/ListAllTasks.java
@@ -2,11 +2,15 @@ package Commands.Listing;
 
 import Commands.Contracts.Command;
 import Core.Contracts.TaskManagementRepository;
+import Models.Tasks.Enums.Status;
 import Models.Tasks.Interfaces.Task;
 import Utils.ListingHelpers;
+import Utils.ParsingHelpers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListAllTasks implements Command {
     private List<Task> tasks;
@@ -19,6 +23,16 @@ public class ListAllTasks implements Command {
         if(tasks.isEmpty()) {
             return "There are no tasks.";
         }
+
+        if (parameters.get(0).equals("filter")) {
+            List<Task> activeTasks = tasks.stream().filter(task -> task.getTitle() == parameters.get(1)).collect(Collectors.toList());
+            return activeTasks.toString();
+        }
+        if(parameters.get(0).equals("sort")){
+            List<Task> sortedTasks = tasks.stream().sorted(Comparator.comparing(task -> task.getTitle())).collect(Collectors.toList());
+            return sortedTasks.toString();
+        }
+
         return ListingHelpers.elementsToString(tasks);
     }
 }
